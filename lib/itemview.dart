779443c5ee70b1a303129ff99f82885a510823b0
jatinmuley <jatinmuley678@gmail.com>
2021-04-15 19:39:00 +0530

    still not fully functional

---
+++ lib/itemview.dart
@@ -0,0 +1,313 @@
+import 'package:bloc_pattern/bloc_pattern.dart';
+import 'package:flutter/material.dart';
+import 'package:new_task/checkoutpage.dart';
+import "checkoutpage.dart";
+import 'bloc/cartlistBloc.dart';
+
+import 'model/fooditem.dart';
+
+
+ 
+class Check extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      blocs: [
+        //add yours BLoCs controlles
+        Bloc((i) => CartListBloc()),
+      //  Bloc((i) => ColorBloc()),
+      ],
+      child: MaterialApp(
+        title: "Food Delivery",
+        home: Home(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Container(
+        child: ListView(
+          children: <Widget>[
+            FirstHalf(),
+            SizedBox(height: 45),
+            for (var foodItem in fooditemList.foodItems)
+              Builder(
+                builder: (context) {
+                  return ItemContainer(foodItem: foodItem);
+                },
+              )
+          ],
+        ),
+      )),
+    );
+  }
+}
+
+class ItemContainer extends StatelessWidget {
+  
+  ItemContainer({
+    @required this.foodItem,
+  });
+
+  final FoodItem foodItem;
+  final CartListBloc bloc = BlocProvider.getBloc<CartListBloc>();
+
+  
+  addToCart(FoodItem foodItem) {
+    bloc.addToList(foodItem);
+  }
+
+  removeFromList(FoodItem foodItem) {
+    bloc.removeFromList(foodItem);
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        addToCart(foodItem);
+        final snackBar = SnackBar(
+          content: Text('${foodItem.title} added to Cart'),
+          duration: Duration(milliseconds: 550),
+        );
+
+        Scaffold.of(context).showSnackBar(snackBar);
+      },
+      child: Items(
+      
+        itemName: foodItem.title,
+        itemPrice: foodItem.price,
+        
+       
+      ),
+    );
+  }
+}
+
+class FirstHalf extends StatelessWidget {
+  const FirstHalf({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(35, 25, 0, 0),
+      child: Column(
+        children: <Widget>[
+          CustomAppBar(),
+          //you could also use the spacer widget to give uneven distances, i just decided to go with a sizebox
+          SizedBox(height: 30),
+         
+          SizedBox(height: 30),
+          
+          SizedBox(height: 45),
+         
+        ],
+      ),
+    );
+  }
+}
+
+
+class Items extends StatelessWidget {
+  Items({
+   
+    @required this.itemName,
+    @required this.itemPrice,
+   
+  });
+
+ 
+  final String itemName;
+  final double itemPrice;
+  
+
+  @override
+  Widget build(BuildContext context) {
+    double containerPadding = 45;
+    double containerBorderRadius = 10;
+
+    return Column(
+      children: <Widget>[
+        Container(
+         
+          child: Column(
+            children: <Widget>[
+            
+              SizedBox(height: 20),
+              Container(
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Expanded(
+                              child: Text(itemName,
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.w700,
+                                    fontSize: 18,
+                                  )),
+                            ),
+                            Text("\Rs $itemPrice",
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w700,
+                                  fontSize: 18,
+                                )),
+                          ],
+                        ),
+                        SizedBox(height: 10),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: RichText(
+                            text: TextSpan(
+                                style: TextStyle(
+                                    color: Colors.black45, fontSize: 15),
+                                children: [
+                                 
+                                ]),
+                          ),
+                        ),
+                        SizedBox(height: containerPadding),
+                      ])),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class CategoryListItem extends StatelessWidget {
+  const CategoryListItem({
+    Key key,
+    @required this.categoryIcon,
+    @required this.categoryName,
+    @required this.availability,
+    @required this.selected,
+  }) : super(key: key);
+
+  final IconData categoryIcon;
+  final String categoryName;
+  final int availability;
+  final bool selected;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(right: 20),
+      padding: EdgeInsets.fromLTRB(10, 10, 10, 20),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(50),
+       // color: selected ? Themes.color : Colors.white,
+        border: Border.all(
+            color: selected ? Colors.transparent : Colors.grey[200],
+            width: 1.5),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey[100],
+            blurRadius: 15,
+            offset: Offset(15, 0),
+            spreadRadius: 5,
+          )
+        ],
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(50),
+                border: Border.all(
+                    color: selected ? Colors.transparent : Colors.grey[200],
+                    width: 1.5)),
+            child: Icon(
+              categoryIcon,
+              color: Colors.black,
+              size: 30,
+            ),
+          ),
+          SizedBox(height: 10),
+          Text(
+            categoryName,
+            style: TextStyle(
+                fontWeight: FontWeight.w700, color: Colors.black, fontSize: 15),
+          ),
+          Container(
+            margin: EdgeInsets.fromLTRB(0, 6, 0, 10),
+            width: 1.5,
+            height: 15,
+            color: Colors.black26,
+          ),
+          Text(
+            availability.toString(),
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+class CustomAppBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final CartListBloc bloc = BlocProvider.getBloc<CartListBloc>();
+    // TODO: implement build
+    return Container(
+      margin: EdgeInsets.only(bottom: 15),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          
+          StreamBuilder(
+            stream: bloc.listStream,
+            builder: (context, snapshot) {
+              List<FoodItem> foodItems = snapshot.data;
+              int length = foodItems != null ? foodItems.length : 0;
+
+              return buildGestureDetector(length, context, foodItems);
+            },
+          )
+        ],
+      ),
+    );
+  }
+
+  GestureDetector buildGestureDetector(
+      int length, BuildContext context, List<FoodItem> foodItems) {
+    return GestureDetector(
+      onTap: () {
+        if (length > 0) {
+          Navigator.push(context,
+             MaterialPageRoute(builder: (context) => checkoutPage()));
+        } else {
+          return;
+        }
+      },
+      child: Container(
+        margin: EdgeInsets.only(right: 30),
+        child: Text(length.toString()),
+        padding: EdgeInsets.all(15),
+        decoration: BoxDecoration(
+            color: Colors.yellow[800], borderRadius: BorderRadius.circular(50)),
+      ),
+    );
+  }
+}
